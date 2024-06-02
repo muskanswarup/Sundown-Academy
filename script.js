2bf74699f1d5bdead7f7eda772293fe241af2432
Muskan Swarup <muskanswarup680@gmail.com>
2024-06-02 21:00:03 +0530

    completed footer and responsiveness

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-
-
 function page5Animation(){
     
     var elemC = document.querySelector("#elem-container");
@@ -36,26 +34,38 @@ function swiperAnimation(){
       });
 }
 
+function menuAnimation(){    
+    var menu = document.querySelector("nav h3");
+    var full = document.querySelector("#full-scr");
+    var navImg = document.querySelector("nav img");
+    var flag = 0;
+
+
+    menu.addEventListener("click" , function(){
+        if(flag == 0){
+            full.style.top = 0;
+            navImg.style.opacity = 0;
+            flag = 1;
+        }else{
+            full.style.top = "-100%";
+            navImg.style.opacity = 1;
+            flag = 0;
+
+        }
+
+    })
+}
+
+function loaderAnimation(){
+    var loader = document.querySelector("#loader");
+
+    setTimeout(function(){
+        loader.style.top = "-100%";
+
+    } , 4200);
+}
+
 swiperAnimation();
 page5Animation();
-
-
-var menu = document.querySelector("nav h3");
-var full = document.querySelector("#full-scr");
-var navImg = document.querySelector("nav img");
-var flag = 0;
-
-
-menu.addEventListener("click" , function(){
-    if(flag == 0){
-        full.style.top = 0;
-        navImg.style.opacity = 0;
-        flag = 1;
-    }else{
-        full.style.top = "-100%";
-        navImg.style.opacity = 1;
-        flag = 0;
-
-    }
-
-})
+menuAnimation();
+loaderAnimation();
